39b5331ce577e56521319baa78a7ead73f0c9930
MasonCrochetiere <133772128+MasonCrochetiere@users.noreply.github.com>
2024-09-10 11:17:03 -0400

    work from class - triangle gaming!!!
    go to learn open gl.com - linking vertex attributes reread and go from there

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -9,6 +9,35 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float vertices[] = {
+	// first triangle
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f,  0.5f, 0.0f,  // top left 
+};
+
+const char* vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+void main()
+{
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); 
+}
+)";
+// this is the source code we are sending to the GPU to run and make the vertex shader
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)";
+// same deal as vertex shader source; code we send to the GPU to generate color. 
+// the variable's name is arbitrary
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -25,7 +54,67 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
 	//Initialization goes here!
+	unsigned int VBO;
+	glGenBuffers(1, &VBO); // populates VBO with the next available buffer indicator
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // sets the buffer to mess with to be VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // so this transfers vertex data from CPU to GPU?
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // telling GPU when to jump in data (how many bytes?) to get to each vertex
+	glEnableVertexAttribArray(0);
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); // similar to VBO, populating vertexShader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // apply our source code to this shader. the 1 means we're only supplying 1 shader
+	glCompileShader(vertexShader); // same as compiling any other code. Could fail...
+
+	int success;
+	char infoLog[512]; // arbitrary number to hold error log
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); 
+	if (!success)
+	{
+		
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
+	}
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); // similar to VBO, populating vertexShader
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); // apply our source code to this shader. the 1 means we're only supplying 1 shader
+	glCompileShader(fragmentShader); // same as compiling any other code. Could fail...
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)	
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram(); // program combines shaders
+	glAttachShader(shaderProgram, vertexShader); 
+	glAttachShader(shaderProgram, fragmentShader); // attach our vertex and color shaders
+	glLinkProgram(shaderProgram); // links shaders all together
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("ERROR::PROGRAM::LINKING_FAILED\n%s", infoLog);
+	}
+
+	glUseProgram(shaderProgram); // need to make sure we use the program :)
+	// consider using this during game loop instead, esp if swapping between multiple program
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	// shader cleanup
+
+	//glBindBuffer(GL_ARRAY_BUFFER, 0); // sets the buffer to mess with to be 0, so nothing
 	
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -33,6 +122,9 @@ int main() {
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		//Drawing happens here!
 		glfwSwapBuffers(window);
 	}
