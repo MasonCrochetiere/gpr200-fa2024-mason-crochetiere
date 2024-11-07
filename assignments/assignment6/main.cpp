b26f00ccd09d5016e6c0122779b469a25546b86d
Reece <Reece.enthoven@mymail.champlain.edu>
2024-11-07 12:02:07 -0500

    initial commit for branch

--- assignments/assignment6/main.cpp
+++ assignments/assignment6/main.cpp
@@ -82,7 +82,7 @@ float vertices[] = {
 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,  0.0f,  1.0f,  0.0f,
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  0.0f,  1.0f,  0.0f
 };
-
+//aahh
 glm::vec3 cubePositions[] = {
 	glm::vec3(0.0f,  0.0f,  0.0f),
 	glm::vec3(2.0f,  5.0f, -15.0f),
